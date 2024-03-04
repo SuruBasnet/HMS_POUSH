6c68624facac3e0430e2aa59ba0315ae97e5fe46
SuruBasnet <surubasnet824@gmail.com>
2024-03-04 07:35:17 +0545

    permission finished

--- HMS/settings.py
+++ HMS/settings.py
@@ -123,7 +123,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES' : [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated',
+        'core.permissions.CustomDjangoModelPermission'
     ]
 }
 
